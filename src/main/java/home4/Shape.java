a333fe1a3dcae5dbfe6f14f92d2fc391a6589b09
playjbodnar <36080598+playjbodnar@users.noreply.github.com>
2018-03-06 22:55:28 +0200

    Added toString in Shape

--- src/main/java/home4/Shape.java
+++ src/main/java/home4/Shape.java
@@ -8,11 +8,15 @@ abstract public class Shape {
     abstract public double caculateSquare();
     abstract public double caculatePerimeter();
 
-    abstract protected void printParameters();
-
-    public void printShape(){
-        System.out.printf("%s with parameters:%n", shapeName);
-        printParameters();
-        System.out.printf("%n  square is %f%n  perimeter is %f",caculateSquare(),caculatePerimeter());
+    abstract protected String getParametersString();
+    
+    @Override
+    public String toString(){
+        StringBuilder shapeString = new StringBuilder();
+        shapeString.append(String.format("%s with parameters:%n", shapeName));
+        shapeString.append(getParametersString);
+        shapeString.append(String.format(""%n  square is %f%n  perimeter is %f",caculateSquare(),caculatePerimeter()));
     }
+    
+    
 }
